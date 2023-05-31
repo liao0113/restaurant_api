532b8836b59e5b2eecfd091e9f6ab320dd4e366f
liao <liao@fakemail.com>
2023-05-31 13:29:51 +0800

    userController complete

--- controllers/userController.js
+++ controllers/userController.js
@@ -8,6 +8,7 @@ const {
 } = require("../models");
 const bcrypt = require("bcryptjs");
 const helpers = require("../_helpers");
+const imgur = require("imgur-node-api");
 
 module.exports = {
   renderLoginPage: (req, res) => {
@@ -55,7 +56,7 @@ module.exports = {
     res.redirect("/login");
   },
   getUserProfile: async (req, res) => {
-    let user = await User.findbyPk(req.params.id, {
+    let currUser = await User.findbyPk(req.params.id, {
       include: [
         Comment,
         { model: Restaurant, as: "FavoritedRestaurants" },
@@ -63,8 +64,8 @@ module.exports = {
         { model: User, as: "Followings" },
       ],
     });
-    const filerRestId = [
-      ...new Set(user.Comments.map((comment) => comment.RestaurantId)),
+    const filterRestId = [
+      ...new Set(currUser.Comments.map((comment) => comment.RestaurantId)),
     ];
     //raw: true,next: ture 把提出的資料變成JSON格式
     const commentRest = await Restaurant.findAll({
@@ -73,11 +74,103 @@ module.exports = {
       where: { id: filterRestId },
       attributes: ["id", "image"],
     });
-    user = {
-      ...user.toJSON(),
+    currUser = {
+      ...currUser.toJSON(),
       identify: Number(req.params.id) === Number(helpers.getUser(req).id),
       CommentedRest: commentRest,
     };
-    res.render("profile", { user });
+    res.render("profile", { currUser });
+  },
+  editUserPage: (req, res) => {
+    if (Number(req.params.id) !== Number(helpers.getUser(req).id)) {
+      return res.redirect("back");
+    } else {
+      return User.findbyPk(req.params.id, { raw: true }).then((user) => {
+        res.render("edit", { user });
+      });
+    }
+  },
+  putUserProflie: async (req, res) => {
+    const { file } = req;
+    if (file) {
+      imgur.setClientID(`${IMGUR_CLIENT_ID}`);
+      imgur.upload(file.path, async (err, img) => {
+        let oldUser = await User.findbyPk(req.params.id);
+        let updateUser = await oldUser.update({
+          name: req.body.name,
+          email: req.body.email,
+          image: file ? img.data.link : oldUser.image,
+        });
+      });
+      req.flash("success", "使用者編輯成功!");
+      return res.redirect(`/users/${req.params.id}`);
+    } else {
+      let oldUser = await User.findbyPk(req.params.id);
+      let updateUser = await oldUser.update({
+        name: req.body.name,
+        email: req.body.email,
+        image: oldUser.image,
+      });
+      req.flash("success", "使用者編輯成功!");
+      return res.redirect(`/users/${req.params.id}`);
+    }
+  },
+  getTopUser: async (req, res) => {
+    let users = await User.findAll({
+      include: [{ model: "User", as: "Followers" }],
+    });
+    users = users.map((user) => ({
+      ...user.toJSON(),
+      followerCount: user.Followers.length,
+      isFollowed: req.user.Followings.map((data) => data.id).includes(user.id),
+    }));
+    users = users.sort((a, b) => b.FollowerCount - a.FollowerCount);
+    return res.render("topUser", { users });
+  },
+  addFavorite: async (req, res) => {
+    await Favorite.create({
+      userId: req.user.id,
+      restaurantId: req.params.restaurantId,
+    });
+    return res.redirect("back");
+  },
+  removeFavorite: async (req, res) => {
+    await Favorite.destroy({
+      where: {
+        userId: req.user.id,
+        restaurantId: req.params.restaurantId,
+      },
+    });
+    return res.redirect("back");
+  },
+  addLike: async (req, res) => {
+    await Like.create({
+      userId: req.user.id,
+      restaurantId: req.params.restaurantId,
+    });
+    return res.redirect("back");
+  },
+  removeLike: async (req, res) => {
+    await Like.destroy({
+      where: {
+        userId: req.user.id,
+        restaurantId: req.params.restaurantId,
+      },
+    });
+    return res.redirect("back");
+  },
+  addFollowing: async (req, res) => {
+    await Followship.create({
+      followerId: req.user.id,
+      followingId: req.params.userId,
+    });
+  },
+  removeFollowing: async (req, res) => {
+    await Followship.destroy({
+      where: {
+        followerId: req.user.id,
+        followingId: req.params.userId,
+      },
+    });
   },
 };
